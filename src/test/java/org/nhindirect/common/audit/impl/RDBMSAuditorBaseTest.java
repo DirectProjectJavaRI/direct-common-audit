2cadaf293d2ce3df488f91542f7def76a4404f7d
greg.meyer <greg.meyer@oracle.com>
2025-08-07 14:44:55 -0500

    Initial RI 9.0 commit.

--- src/test/java/org/nhindirect/common/audit/impl/RDBMSAuditorBaseTest.java
+++ src/test/java/org/nhindirect/common/audit/impl/RDBMSAuditorBaseTest.java
@@ -2,16 +2,11 @@ package org.nhindirect.common.audit.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
-
-
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.nhindirect.common.audit.R2DBCTestConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = R2DBCTestConfiguration.class)
 @TestPropertySource("classpath:bootstrap.properties")
 public abstract class RDBMSAuditorBaseTest 
